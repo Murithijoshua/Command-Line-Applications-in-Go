f391e5980eaa551c1de9fc7b04aae9e3de31d9d5
murithijoshua <brodiamyzalius@gmail.com>
2023-07-25 21:19:04 +0300

    cli changes
    Signed-off-by: murithijoshua <brodiamyzalius@gmail.com>

--- main.go
+++ main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+type Person struct {
+	firstname string
+	lastname  string
+	age       int
+}
+
 func main() {
-	fmt.Println("hello world in go!")
+	// Creating a pointer to a new zero-value instance of the Person struct.
+	personPtr := new(Person)
+
+	// Accessing and modifying fields of the struct.
+	personPtr.firstname = "Jane"
+	personPtr.lastname = "Smith"
+	personPtr.age = 25
+
+	// Accessing the fields of the struct using the pointer.
+	fmt.Println("First Name:", personPtr.firstname)
+	fmt.Println("Last Name:", personPtr.lastname)
+	fmt.Println("Age:", personPtr.age)
 }
--- todo/cmd/todo/main.go
+++ todo/cmd/todo/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"joshua.com/todo"
 	"os"
@@ -10,12 +11,39 @@ import (
 const TodoFile = "./todo.json"
 
 func main() {
+	TaskName := flag.String("task", "", "task-name")
+	completed := flag.Int("complete", 0, "completed name")
+	list := flag.Bool("list", false, "List all task")
+	fmt.Println(TaskName, completed)
 	l := &todo.List{}
 	if err := l.Get(TodoFile); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 	switch {
+	case *list:
+		for _, item := range *l {
+			if !item.Done {
+				fmt.Println(item.Task)
+			}
+		}
+	case *completed > 0:
+		if err := l.Complete(*completed); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		if err := l.Save(TodoFile); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	case *task != "":
+		l.Add(*task)
+
+		if err := l.Save(TodoFile); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
 	// For no extra arguments, print the list
 	case len(os.Args) == 1:
 		// List current to do items
